c73d7411a08560c60e56a6d4f09828b6e81e8d07
Arul <220395395+ArulE23@users.noreply.github.com>
2026-06-24 08:19:07 +1000

    Add colour trail to grid

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 const sketchGrid = document.querySelector("#sketch-grid");
 
+const colourCellBlack = (event) => {
+  event.target.style.backgroundColor = "black";
+}
+
 function initGrid(size) {
+  sketchGrid.replaceChildren();
   for (let i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.style.display = "flex";
     row.style.flex = "1";
     sketchGrid.appendChild(row);
-    console.log(i);
   }
   for (child of document.querySelectorAll("#sketch-grid > div")) {
     for (let i = 0; i < size; i++) {
       const cell = document.createElement("div");
       cell.style.flex = "1";
-      cell.style.border = "1px solid grey";
+      cell.style.border = "0.5px solid grey";
       child.appendChild(cell);
+      cell.addEventListener("mouseover", colourCellBlack);
     }
   }
 }
